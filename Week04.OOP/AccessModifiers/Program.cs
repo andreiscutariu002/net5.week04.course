030321d986b5e8538e55aab4705486624d20531d
ascutariu <andreiscutariu92@gmail.com>
2019-10-29 19:32:26 +0200

    add access modifiers project

---
+++ Week04.OOP/AccessModifiers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Student
+    {
+        string cnp;
+
+        // prop pt cnp
+        public string Cnp
+        {
+            get { return cnp; }
+            set { cnp = value; }
+        }
+
+        // auto property
+        public string Name { get; set; }
+
+        // dinamyc property & readonly
+        public string FullDescription
+        {
+            get { return $"{cnp} - {Name}"; }
+        }
+    }
+}
