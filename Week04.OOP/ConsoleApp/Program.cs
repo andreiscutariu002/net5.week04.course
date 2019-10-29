3d27fedc25950f5aa46cc3ad868c64e22da6d121
ascutariu <andreiscutariu92@gmail.com>
2019-10-29 18:04:23 +0200

    add procedural code

--- Week04.OOP/ConsoleApp/Program.cs
+++ Week04.OOP/ConsoleApp/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace ConsoleApp
+﻿using System;
+
+namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello");
+            System.Console.WriteLine("Hello from program ...");
+
+            int a = ReadNumber();
+            int b = ReadNumber();
+
+            int difResult = Dif(a, b);
+
+            Console.WriteLine($"{a} - {b} = {difResult}");
+
+            int addResult = Add(a, b);
+
+            Console.WriteLine($"{a} + {b} = {difResult}");
+        }
+
+        public static int ReadNumber()
+        {
+            Console.Write("Enter a number: ");
+
+            string input = Console.ReadLine();
+
+            return int.Parse(input);
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Dif(int a, int b)
+        {
+            return a - b;
         }
     }
 }
